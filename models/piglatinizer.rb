528d5a4b34470caea3553eb16e45ed53323c0722
David McGrann <dpmcgrann@gmail.com>
2018-08-15 20:25:46 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,10 @@ class PigLatinizer
       elsif vowel.include? p[0].downcase
         "#{p}way"
       else
-        split = word.s
+        pig_word = p.split(/([aeiou])/,2)
+        "#{pig_word[1]}#{pig_word[2]}#{pig_word[0]}ay"
+      end
+    end.join(" ")
   end
   
 end
