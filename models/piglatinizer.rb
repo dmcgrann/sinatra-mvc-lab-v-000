5ff954dd279b8b586e2a6ac529b67aac7b8ca088
David McGrann <dpmcgrann@gmail.com>
2018-08-15 20:22:15 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,16 @@
 class PigLatinizer
-  attr_accessor :words
   
-  
-  def initialize(words="")
-    @words = words
-  end
-
-
   def piglatinize(word)
+    @word = word.downcase
+    vowel = "aeiou"
+    word = word.split()
+    word.map do |p|
+      if p.length == 1 
+        "#{p}way"
+      elsif vowel.include? p[0].downcase
+        "#{p}way"
+      else
+        split = word.s
   end
   
 end
