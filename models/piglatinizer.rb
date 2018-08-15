98dc3a1d9949930f279a61326002f6e2ab697802
David McGrann <dpmcgrann@gmail.com>
2018-08-15 20:01:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
   
   def piglatinize(word)
   if word =~ (/\A[aeiou]/i)
-    word = word + 'way'
+    word = word + 'ay'
   elsif word =~ (/\A[^aeiou]/i)
     match = /\A[^aeiou]/i.match(word)
     word = match.post_match + match.to_s + 'way'
