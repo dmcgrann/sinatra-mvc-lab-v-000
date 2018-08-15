82948520841057932e71f29316e79d108e59c5cd
David McGrann <dpmcgrann@gmail.com>
2018-08-15 20:00:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,9 +5,14 @@ class PigLatinizer
     @words = words
   end
   
-  def piglatinize(input)
-    input = @words.reverse + "ay"
-    
+  def piglatinize(word)
+  if word =~ (/\A[aeiou]/i)
+    word = word + 'ay'
+  elsif word =~ (/\A[^aeiou]/i)
+    match = /\A[^aeiou]/i.match(word)
+    word = match.post_match + match.to_s + 'ay'
   end
+  word
+end
   
 end
